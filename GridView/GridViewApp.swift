d079955d30b6529a5a17601b1ade0d0ae8757bec
蘇健豪 <jenhausu@osensetech.com>
2022-02-15 11:01:47 +0800

    Initial Commit

---
+++ GridView/GridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewApp.swift
+//  GridView
+//
+//  Created by 蘇健豪 on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct GridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
